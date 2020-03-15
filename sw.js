8a3e07a2d55ce77f36f177cba14f817cc8a1e43d
connoringold <connoringold@gmail.com>
2020-03-15 19:59:25 +0000

    attempt to fix PWA bug

--- sw.js
+++ sw.js
@@ -7,11 +7,11 @@ self.addEventListener("install", e => {
         .addAll([
           // first url is important (check network tab)
           "/",
-          "app.js",
-          "index.html",
-          "js/function.js",
-          "css/index.css",
-          "img/placeholder.jpg",
+          "/app.js",
+          "/index.html",
+          "/js/function.js",
+          "/css/index.css",
+          "/img/placeholder.jpg",
           "https://fonts.googleapis.com/css?family=Heebo:300|Playfair+Display:400,700&display=swap"
         ])
         .then(() => self.skipWaiting());
