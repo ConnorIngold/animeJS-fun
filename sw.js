cec6dd9ef170c27a2e35a362eaa168c27a043498
connoringold <connoringold@gmail.com>
2020-03-15 19:30:33 +0000

    attempt to fix PWA bug

--- sw.js
+++ sw.js
@@ -1,36 +1,34 @@
-var CACHE_NAME = "my-site-cache-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "js/function.js",
-  "js/app.js",
-  "css/index.css",
-  "img/placeholder.jpg",
-  "https://fonts.googleapis.com/css?family=Heebo:300|Playfair+Display:400,700&display=swap"
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(function(cache) {
-        console.log(
-          "Opened cache"
-        );
-        return cache.addAll(assets);
-      })
+const version = "0.6.18";
+const cacheName = `airhorner-${version}`;
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache
+        .addAll([
+          "/",
+          "/index.html",
+          "/js/function.js",
+          "/js/app.js",
+          "/css/index.css",
+          "/img/placeholder.jpg",
+          "https://fonts.googleapis.com/css?family=Heebo:300|Playfair+Display:400,700&display=swap"
+        ])
+        .then(() => self.skipWaiting());
+    })
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+    caches
+      .open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
